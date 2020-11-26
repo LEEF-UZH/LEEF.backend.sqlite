f98290993cf528f4674e51f6c208c535c19612d5
Rainer M. Krug <Rainer@krugs.de>
2020-11-26 11:31:35 +0100

    Change additor to multiple dbs

--- R/register.R
+++ R/register.R
@@ -8,7 +8,7 @@ register <- function() {
   if (is.null(system.file(package = "LEEF"))) {
     stop("This function requres the package to be installed!")
   }
-  LEEF::add_additor( additor_sqlite )
+  LEEF::add_additor( additor_sqlite_multiple_db )
   ##
   invisible(TRUE)
 }
